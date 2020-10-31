f2c954fdd37cddd30c102dc3e57be6217f2459fb
YYarkin <Yarkin73@gmail.com>
2020-10-31 14:19:57 +0300

    training

---
+++ src/tests/ArticleTests.java
@@ -0,0 +1,40 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class ArticleTests extends CoreTestCase {
+
+    @Test
+    public void testCompareArticleTitle() {
+        SearchPageObject searchPageObject = new SearchPageObject(driver);
+        ArticlePageObject articlePageObject = new ArticlePageObject(driver);
+
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchLine("Java");
+        searchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        String articleTitle = articlePageObject.getArticleTitle();
+
+        assertEquals(
+                "We see unexpected title",
+                "Java (programming language)",
+                articleTitle
+        );
+    }
+
+    @Test
+    public void testSwipeArticle() {
+        SearchPageObject searchPageObject = new SearchPageObject(driver);
+        ArticlePageObject articlePageObject = new ArticlePageObject(driver);
+
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchLine("Appium");
+        searchPageObject.clickByArticleWithSubstring("Appium");
+
+        articlePageObject.waitForTitleElement();
+        articlePageObject.swipeToFooter();
+    }
+}
---
+++ src/tests/MyListsTests.java
@@ -0,0 +1,34 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.MyListsPageObject;
+import lib.ui.NavigationUI;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class MyListsTests extends CoreTestCase {
+
+    @Test
+    public void testSaveFirstArticleToMyList() {
+        SearchPageObject searchPageObject = new SearchPageObject(driver);
+        ArticlePageObject articlePageObject = new ArticlePageObject(driver);
+        NavigationUI navigationUI = new NavigationUI(driver);
+        MyListsPageObject myListsPageObject = new MyListsPageObject(driver);
+
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchLine("Java");
+        searchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        articlePageObject.waitForTitleElement();
+        String articleTitle = articlePageObject.getArticleTitle();
+        String nameOfFolder = "Learning programming";
+        articlePageObject.addArticleToMyList(nameOfFolder);
+        articlePageObject.closeArticle();
+
+        navigationUI.clickMyLists();
+
+        myListsPageObject.openFolderByName(nameOfFolder);
+        myListsPageObject.swipeByArticleToDelete(articleTitle);
+    }
+}
