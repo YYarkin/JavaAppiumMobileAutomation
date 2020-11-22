b91ed3b101f0edd04b38cf9b67fbf43b03f7a685
YYarkin <Yarkin73@gmail.com>
2020-11-22 09:59:47 +0300

    locator up

--- src/main/java/lib/ui/ArticlePageObject.java
+++ src/main/java/lib/ui/ArticlePageObject.java
@@ -9,7 +9,7 @@ public class ArticlePageObject extends MainPageObject {
             TITLE = "id:org.wikipedia:id/view_page_title_text",
             FOOTER_ELEMENT = "xpath://*[@text='View page in browser']",
             OPTIONS_BUTTON = "xpath://android.widget.ImageView[@content-desc='More options']",
-            OPTIONS_ADD_TO_MY_LIST_BUTTON = "xpath://android.widget.TextView[@text='Add to reading list'][@instance='2']",
+            OPTIONS_ADD_TO_MY_LIST_BUTTON = "xpath://android.widget.TextView[@text='Add to reading list']",
             ADD_TO_MY_LIST_OVERLAY = "id:org.wikipedia:id/onboarding_button",
             MY_LIST_NAME_INPUT = "id:org.wikipedia:id/text_input",
             MY_LIST_OK_BUTTON = "xpath://*[@text='OK']",
